e19240d043b87817b64944577936aabc6c143239
strixik <strixik1985@gmail.com>
2019-12-10 00:22:15 +0200

    fix

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -38,7 +38,7 @@ AppAsset::register($this);
     echo Nav::widget([
         'options' => ['class' => 'navbar-nav navbar-right'],
         'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
+            ['label' => 'Search IIN', 'url' => ['/site/index']],
             ['label' => 'Documents', 'url' => ['site/view']],
            ]
     ]);
